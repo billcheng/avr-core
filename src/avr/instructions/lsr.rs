9adf89c52604948612ea8130dc316386235139f6
Bill Cheng <cccheng118@yahoo.com>
2022-02-07 12:04:21 -0600

    Add LSR

--- src/avr/instructions/lsr.rs
+++ src/avr/instructions/lsr.rs
@@ -18,10 +18,20 @@ impl Instruction for Lsr {
     let rd = registers.get(self.d);
     let result = rd >> 1;
 
+    let carry = rd & 0x01 != 0;
+    let negative = false;
+    let zero = result == 0;
+    let overflow = negative ^ carry;
+    let sign = negative ^ overflow;
+
     registers.set(self.d, result);
 
     let mut status_register = execution_data.status_register.borrow_mut();
-    status_register.set_carry(rd & 0x01 != 0);
+    status_register.set_sign(sign);
+    status_register.set_overflow(overflow);
+    status_register.set_negative(negative);
+    status_register.set_zero(zero);
+    status_register.set_carry(carry);
 
     None
   }
@@ -47,6 +57,10 @@ mod test {
     let status_register = testbed.status_register.borrow();
     assert_eq!(registers.get(31), 0x7f);
     assert_eq!(status_register.get_carry(), 1);
+    assert_eq!(status_register.get_zero(), 0);
+    assert_eq!(status_register.get_negative(), 0);
+    assert_eq!(status_register.get_overflow(), 1);
+    assert_eq!(status_register.get_sign(), 1);
   }
 
   #[test]
@@ -64,5 +78,30 @@ mod test {
     let status_register = testbed.status_register.borrow();
     assert_eq!(registers.get(31), 0x7f);
     assert_eq!(status_register.get_carry(), 0);
+    assert_eq!(status_register.get_zero(), 0);
+    assert_eq!(status_register.get_negative(), 0);
+    assert_eq!(status_register.get_overflow(), 0);
+    assert_eq!(status_register.get_sign(), 0);
+  }
+
+  #[test]
+  fn lsr_r31_0x01_returns_0x00() {
+    let testbed = init(vec![(31, 0x01)]);
+
+    let op = super::Lsr::new(0b1001_0101_1111_0110);
+    op.execute(super::InstructionData {
+      registers: testbed.registers.clone(),
+      status_register: testbed.status_register.clone(),
+      ..testbed
+    });
+
+    let registers = testbed.registers.borrow();
+    let status_register = testbed.status_register.borrow();
+    assert_eq!(registers.get(31), 0x00);
+    assert_eq!(status_register.get_carry(), 1);
+    assert_eq!(status_register.get_zero(), 1);
+    assert_eq!(status_register.get_negative(), 0);
+    assert_eq!(status_register.get_overflow(), 1);
+    assert_eq!(status_register.get_sign(), 1);
   }
 }
