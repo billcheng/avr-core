ba314173ab20ce1b1d76cbfaec741be76b347172
Bill Cheng <cccheng118@yahoo.com>
2022-02-11 13:19:43 -0600

    Fix Ldi instruction

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut core = Core::new(CoreType::Bits16, 8192, 8192);
     core.load_code(&vec![0b0001_1100_0000_0000]);
     let (x, y, z) = core.disassemble(0);
-    println!("{:?} {:?} {:?}", x, y, z);
+    println!("{:4x} {:?} {:?} {:?}", 0, x, y, z);
     // core.single_step();
     // println!("{:?}", core.registers);
 }
